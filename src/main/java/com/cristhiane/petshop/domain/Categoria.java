a91f4756d14a2f040f920a44649a2d97388e97fc
Cristhiane Jacques <cjn_1707@hotmail.com>
2022-01-18 13:15:50 -0300

    Configuração do banco de dados H2 e refletindo classe Categoria no banco de dados

--- src/main/java/com/cristhiane/petshop/domain/Categoria.java
+++ src/main/java/com/cristhiane/petshop/domain/Categoria.java
@@ -3,18 +3,25 @@ package com.cristhiane.petshop.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 // Nesta classe está anotado tudo o que precisa ser feito dentro das classes de domínio de uma aplicação que usa o Spring Framework
 
 // O passo a passo anotado dentro desta classe deve ser seguido por todas as classes de domínio!
 
 // É necessário fazer tudo isso porque estamos usando um framework, dessa forma precisamos seguir as regras dele para que ele já possa nos entregar algumas coisas prontas
 
-//PASSO 4: Classe deve implementar a interface Serializable e deve ter o atributo serial version
+@Entity //marcando a classe como Entity para o JPA mapear ela para o banco de dados
 public class Categoria implements Serializable  {
-
+	//PASSO 4: Classe deve implementar a interface Serializable e deve ter o atributo serial version
 	private static final long serialVersionUID = 1L;
 	
 	// PASSO 1: Criar os atributos
+	@Id // fazendo com que o atributo id seja a chave primária
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //definindo a estratégia de geração automática de valor dessa coluna
 	private Integer id;
 	private String nome;
 	
