37f785cd583db8bb6d3e8cfa1c260be6ba5934d1
Cristhiane Jacques <cjn_1707@hotmail.com>
2022-01-20 15:25:34 -0300

    POST, PUT e DELETE de pessoas e serviços; GET de todas as pessoas

--- src/main/java/com/cristhiane/petshop/resources/PessoaResource.java
+++ src/main/java/com/cristhiane/petshop/resources/PessoaResource.java
@@ -1,11 +1,16 @@
 package com.cristhiane.petshop.resources;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cristhiane.petshop.domain.Pessoa;
 import com.cristhiane.petshop.service.PessoaService;
@@ -25,5 +30,48 @@ public class PessoaResource {
 		Pessoa obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj); // se deu tudo certo, retorna o obj dentro da resposta da requisição
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)// quando eu fizer um POST /pessoas/, o fluxo vem pra esse método aqui embaixo
+	public ResponseEntity<?> insert(@RequestBody Pessoa obj) {// @RequestBody permite pegar o corpo da requisição e passar para dentro do método
+		// o <?> depois do ResponseEntity quer dizer que qualquer tipo de objeto pode ser retornado
+		// o ResponseEntity permite devolver para o cliente uma resposta mais completa
+		
+		obj = service.insert(obj);
+		
+		//criando a URI para acessar o recurso recém criado (essa URI estará disponível no cabeçalho da resposta):
+		// vai retornar algo assim, por exemplo: http://localhost:8080/pessoas/5 (sendo 5 o id da pessoa recém criada)
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build(); //retorna status code 201 e no cabeçalho da resposta informa a uri do recurso recém criado
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)// quando eu fizer um PUT /pessoas/id, o fluxo vem pra esse método aqui embaixo
+	public ResponseEntity<Void> update(@RequestBody Pessoa obj, @PathVariable Integer id) {
+		// o <void> depois do ResponseEntity quer dizer que nada será retornado no corpo da resposta
+		// o ResponseEntity permite devolver para o cliente uma resposta mais completa
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)// quando eu fizer um DELETE /Pessoas/id, o fluxo vem pra esse método aqui embaixo
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		// o <void> depois do ResponseEntity quer dizer que nada será retornado no corpo da resposta
+		// o ResponseEntity permite devolver para o cliente uma resposta mais completa
+		service.delete(id); // esse método pode retornar uma DataIntegrityException, mas quem vai lidar com isso é a classe ResourceExceptionHandler
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)// quando eu fizer um GET /pessoas, o fluxo vem pra esse método aqui embaixo
+	public ResponseEntity<List<Pessoa>> findAll() {
+		// o <?> depois do ResponseEntity quer dizer que qualquer tipo de objeto pode ser retornado
+		// o ResponseEntity permite devolver para o cliente uma resposta mais completa
+		
+		List<Pessoa> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list); // se deu tudo certo, retorna a lista de pessoas dentro da resposta da requisição
 	}
 }
--- src/main/java/com/cristhiane/petshop/service/PessoaService.java
+++ src/main/java/com/cristhiane/petshop/service/PessoaService.java
@@ -1,12 +1,15 @@
 package com.cristhiane.petshop.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.cristhiane.petshop.domain.Pessoa;
 import com.cristhiane.petshop.repository.PessoaRepository;
+import com.cristhiane.petshop.service.exceptions.DataIntegrityException;
 import com.cristhiane.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 @Service
@@ -19,6 +22,30 @@ public class PessoaService {
 		// Fazendo dessa forma, lidamos de forma mais elegante com o caso de não ser retornado nada do repository, evitando o uso de try-catch
 		Optional<Pessoa> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjetoNaoEncontradoException("Objeto não encontrado. ID: " + id + ", Tipo: " + Pessoa.class.getName()));
+	}
+	
+	public Pessoa insert(Pessoa obj) {
+		obj.setId(null); // tanto o POST quanto o PUT usarão em seus services o save para gravar no banco de dados; a diferença é que no POST a gente tem que garantir que o id do objeto seja nulo para que ele seja inserido no banco
+		return repo.save(obj); //quando formos implementar o service do PUT usaremos o save também
+	}
+	
+	public Pessoa update(Pessoa obj) {
+		find(obj.getId()); // antes de pedir pro repository salvar, é preciso verificar se o registro existe: aqui reutilizamos o método find, criado logo acima, que já retorna uma exceção se não achar o objeto
+		return repo.save(obj); // se não estourar exceção ali em cima, o objeto existe, então é só mandar salvar no banco
+	}
+	
+	public void delete(Integer id) {
+		find(id); // antes de pedir pro repository salvar, é preciso verificar se o registro existe: aqui reutilizamos o método find, criado logo acima, que já retorna uma exceção se não achar o objeto
+		try {
+			repo.deleteById(id); // se não estourar exceção ali em cima, o objeto existe, então é só mandar deletar do banco (caso não dê exceção aqui dentro)
+		} catch (DataIntegrityViolationException e) {// DataIntegrityViolationException é a exceção lançada quando se tenta deletar uma Pessoa
+			throw new DataIntegrityException("Pessoa não pode ser deletada!");
+		}
+		
+	}
+
+	public List<Pessoa> findAll() {
+		return repo.findAll();
 	}
 
 }
